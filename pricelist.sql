72d16655bdb5cc6df8ff993d043340736a8ed5b1
Akhilesh-Kasaudhan <akhil2003kd@gmail.com>
2025-08-30 02:28:06 +0530

    _redirects setup done

---
+++ pricelist.sql
@@ -0,0 +1,23 @@
+INSERT INTO "Pricelists" 
+(article_no, product_name, in_price, price, unit, in_stock, description, "createdAt", "updatedAt")
+VALUES
+('A001', 'Product 1', 10.00, 15.00, 'pcs', 51, 'Description of test product 1', NOW(), NOW()),
+('A002', 'Product 2', 11.00, 16.00, 'pcs', 52, 'Description of test product 2', NOW(), NOW()),
+('A003', 'Product 3', 12.00, 17.00, 'pcs', 53, 'Description of test product 3', NOW(), NOW()),
+('A004', 'Product 4', 13.00, 18.00, 'pcs', 54, 'Description of test product 4', NOW(), NOW()),
+('A005', 'Product 5', 14.00, 19.00, 'pcs', 55, 'Description of test product 5', NOW(), NOW()),
+('A006', 'Product 6', 15.00, 20.00, 'pcs', 56, 'Description of test product 6', NOW(), NOW()),
+('A007', 'Product 7', 16.00, 21.00, 'pcs', 57, 'Description of test product 7', NOW(), NOW()),
+('A008', 'Product 8', 17.00, 22.00, 'pcs', 58, 'Description of test product 8', NOW(), NOW()),
+('A009', 'Product 9', 18.00, 23.00, 'pcs', 59, 'Description of test product 9', NOW(), NOW()),
+('A010', 'Product 10', 19.00, 24.00, 'pcs', 60, 'Description of test product 10', NOW(), NOW()),
+('A011', 'Product 11', 20.00, 25.00, 'pcs', 61, 'Description of test product 11', NOW(), NOW()),
+('A012', 'Product 12', 21.00, 26.00, 'pcs', 62, 'Description of test product 12', NOW(), NOW()),
+('A013', 'Product 13', 22.00, 27.00, 'pcs', 63, 'Description of test product 13', NOW(), NOW()),
+('A014', 'Product 14', 23.00, 28.00, 'pcs', 64, 'Description of test product 14', NOW(), NOW()),
+('A015', 'Product 15', 24.00, 29.00, 'pcs', 65, 'Description of test product 15', NOW(), NOW()),
+('A016', 'Product 16', 25.00, 30.00, 'pcs', 66, 'Description of test product 16', NOW(), NOW()),
+('A017', 'Product 17', 26.00, 31.00, 'pcs', 67, 'Description of test product 17', NOW(), NOW()),
+('A018', 'Product 18', 27.00, 32.00, 'pcs', 68, 'Description of test product 18', NOW(), NOW()),
+('A019', 'Product 19', 28.00, 33.00, 'pcs', 69, 'Description of test product 19', NOW(), NOW()),
+('A020', 'Product 20', 29.00, 34.00, 'pcs', 70, 'Description of test product 20', NOW(), NOW());
