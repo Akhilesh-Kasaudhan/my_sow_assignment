31237b2b406d47c38c45952ae42e990c2b3a798d
Akhilesh-Kasaudhan <akhil2003kd@gmail.com>
2025-08-30 01:03:54 +0530

    postgres setup on server

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,8 @@
 module.exports = {
   DB_NAME: "sow_db_057m",
-  DB_USER: "postgres",
+  DB_USER: "postgress",
   DB_PASS: "4wzelHogWTCfT8DNJmhbv1aFAdI2pUkI",
   DB_HOST: "dpg-d2ovlk2dbo4c73c3plb0-a",
   DB_DIALECT: "postgres",
+  DB_PORT: 5432,
 };
