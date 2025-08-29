edb57abc035b1483b50808c31b95c89914f63e3c
Akhilesh-Kasaudhan <akhil2003kd@gmail.com>
2025-08-30 01:49:08 +0530

    port and db host setup

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,13 @@ fastify.decorate("db", db);
 
 fastify.register(require("./routes/pricelistRoute.js"));
 fastify.register(require("./routes/termsRoutes.js"));
+const PORT = 3000;
 
 const start = async () => {
   try {
     await db.sequelize.sync();
-    await fastify.listen({ port: 3000 });
-    fastify.log.info(`Server listening on http://localhost:3000`);
+    await fastify.listen({ port: PORT, host: "0.0.0.0" });
+    fastify.log.info(`Server listening on http://0.0.0.0:${PORT}`);
   } catch (error) {
     fastify.log.error(error);
     process.exit(1);
