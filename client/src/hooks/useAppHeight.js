aa5b9222ce2e4db448ed4a331ee5038a72aebacc
Akhilesh-Kasaudhan <akhil2003kd@gmail.com>
2025-08-31 00:47:42 +0530

    used custom hook useAppHeight

--- client/src/hooks/useAppHeight.js
+++ client/src/hooks/useAppHeight.js
@@ -3,21 +3,21 @@ import { useEffect } from "react";
 export default function useAppHeight() {
   useEffect(() => {
     const setAppHeight = () => {
-      const height = window.innerHeight;
+      const height = window.visualViewport?.height || window.innerHeight;
       document.documentElement.style.setProperty("--app-height", `${height}px`);
     };
 
-    // Set on load
     setAppHeight();
 
-    // Events to catch browser bar changes
     window.addEventListener("resize", setAppHeight);
     window.addEventListener("orientationchange", setAppHeight);
+    window.addEventListener("scroll", setAppHeight);
     document.addEventListener("visibilitychange", setAppHeight);
 
     return () => {
       window.removeEventListener("resize", setAppHeight);
       window.removeEventListener("orientationchange", setAppHeight);
+      window.removeEventListener("scroll", setAppHeight);
       document.removeEventListener("visibilitychange", setAppHeight);
     };
   }, []);
